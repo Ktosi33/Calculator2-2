f149a9ea78a2a6bbbf024d5577448391d3769275
Konrad Łatwiński <konrad.latwinski@gmail.com>
2023-10-03 12:58:09 +0200

    Initial Commit

---
+++ Calculator2-2/Calculator2_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator2_2App.swift
+//  Calculator2-2
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Calculator2_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
