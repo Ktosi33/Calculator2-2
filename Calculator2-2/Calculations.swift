4cf079fefb1dc8750bdc19bab21d0d5e16df5907
Konrad Łatwiński <konrad.latwinski@gmail.com>
2023-10-03 13:45:31 +0200

    feat: CalcButton, OpButton, Calculations struct

---
+++ Calculator2-2/Calculations.swift
@@ -0,0 +1,8 @@
+//
+//  Calculations.swift
+//  Calculator2-2
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import Foundation
---
+++ Calculator2-2/OpButton.swift
@@ -0,0 +1,18 @@
+//
+//  OpButton.swift
+//  Calculator2-2
+//
+//  Created by student on 18/03/1445 AH.
+//
+
+import SwiftUI
+
+struct OpButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpButton()
+}
